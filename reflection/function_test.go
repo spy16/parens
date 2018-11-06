b8086ae2db09addf741ab7a6558e659db6b1e51e
Shivaprasad <shiv.ylp@gmail.com>
2018-11-06 22:25:33 +0530

    Added benchmark for variadic and non-variadic function calls

--- reflection/function_test.go
+++ reflection/function_test.go
@@ -7,20 +7,57 @@ import (
 	"github.com/spy16/parens/reflection"
 )
 
-func add(a, b int) int {
+func add2(a, b int) int {
 	return a + b
 }
 
-var fVal = reflect.ValueOf(add)
-
-func BenchmarkReflectionCall(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		reflection.Call(fVal, 1, 2)
+func addAll(vals ...float64) float64 {
+	sum := 0.0
+	for _, val := range vals {
+		sum += val
 	}
+	return sum
 }
 
-func BenchmarkNormalCall(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		add(1, 2)
-	}
+var addFunc = reflect.ValueOf(add2)
+var sumFunc = reflect.ValueOf(addAll)
+
+func BenchmarkNonVariadicCall(suite *testing.B) {
+	suite.Run("Normal", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			add2(1, 2)
+		}
+	})
+
+	suite.Run("Reflection", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			reflection.Call(addFunc, 1, 2)
+		}
+	})
+
+	suite.Run("WithTypeConversion", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			reflection.Call(addFunc, []interface{}{int(1), int(2)}...)
+		}
+	})
+}
+
+func BenchmarkVariadicCall(suite *testing.B) {
+	suite.Run("Normal", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			addAll(1, 2)
+		}
+	})
+
+	suite.Run("Reflection", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			reflection.Call(sumFunc, 1, 2)
+		}
+	})
+
+	suite.Run("WithTypeConversion", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			reflection.Call(sumFunc, []interface{}{int(1), int(2)}...)
+		}
+	})
 }
