00eddd20ed2b5dc8409fc132b27b5f2ba59995ad
Shivaprasad <shiv.ylp@gmail.com>
2018-11-08 15:25:36 +0530

    Updated threading example

--- examples/threading_macros.lisp
+++ examples/threading_macros.lisp
@@ -9,8 +9,14 @@
   (square)
   (println " is the square of the summation"))
 
+; above expression without threading-first macro:
+(println (square (+ 1 2)) " is the square of the summation")
+
 ; thread-last macro
 (->>
   (+ 1 2)
   (square) ; since square takes only one argument, last/first are same
   (println "Square of the summation is "))
+
+; above expression without threading-last macro:
+(println "Square of the summation is " (square (+ 1 2)))
