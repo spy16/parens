673b127ce2f4aedaed0cfb2567b2e817dcffa8d1
Shivaprasad <shiv.ylp@gmail.com>
2018-12-20 12:57:37 +0530

    Merge different expr files into one file

---
+++ parser/exprs.go
@@ -0,0 +1,270 @@
+package parser
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/spy16/parens/reflection"
+)
+
+// MacroFunc will receive un-evaluated list of s-expressions and the
+// current scope. In addition, if the macro was accessed through a name
+// the name will be passed as well. If the macro was not accessed by name
+// (e.g. was result of another list etc.), name will be empty string.
+type MacroFunc func(scope Scope, name string, exprs []Expr) (interface{}, error)
+
+// ScopedFunc are like normal functions but get access to the current scope.
+// Eval is an example of a ScopedFunc.
+type ScopedFunc func(scope Scope, vals ...interface{}) (interface{}, error)
+
+// ModuleExpr represents a list of Exprs.
+type ModuleExpr struct {
+	Name  string
+	Exprs []Expr
+}
+
+// Eval executes each expression in the module and returns the last result.
+func (me ModuleExpr) Eval(scope Scope) (interface{}, error) {
+	var val interface{}
+	var err error
+
+	for _, expr := range me.Exprs {
+		val, err = expr.Eval(scope)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return val, nil
+}
+
+func (me ModuleExpr) String() string {
+	strs := []string{}
+	for _, expr := range me.Exprs {
+		strs = append(strs, fmt.Sprint(expr))
+	}
+	return strings.Join(strs, "\n")
+}
+
+// NumberExpr represents number s-expression.
+type NumberExpr struct {
+	NumStr string
+	Number interface{}
+}
+
+// Eval for a number returns itself.
+func (ne NumberExpr) Eval(scope Scope) (interface{}, error) {
+	if ne.Number == nil {
+		num, err := strconv.ParseFloat(ne.NumStr, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		ne.Number = num
+	}
+
+	return ne.Number, nil
+}
+
+func (ne NumberExpr) String() string {
+	return fmt.Sprint(ne.NumStr)
+}
+
+// StringExpr represents single and double quoted strings.
+type StringExpr struct {
+	Value string
+}
+
+// Eval returns unquoted version of the STRING token.
+func (se StringExpr) Eval(_ Scope) (interface{}, error) {
+	return se.Value, nil
+}
+
+// QuoteExpr implements the quote-literal form.
+type QuoteExpr struct {
+	Expr Expr
+}
+
+// Eval returns the expression itself without evaluating it.
+func (qe QuoteExpr) Eval(scope Scope) (interface{}, error) {
+	return qe.Expr, nil
+}
+
+// UnquoteEval unquotes and evaluates the underlying expression.
+func (qe QuoteExpr) UnquoteEval(scope Scope) (interface{}, error) {
+	return qe.Expr.Eval(scope)
+}
+
+func (qe QuoteExpr) String() string {
+	return fmt.Sprintf("'%s", qe.Expr)
+}
+
+// CommentExpr is returned to represent a lisp-style comment.
+type CommentExpr struct {
+	comment string
+}
+
+// Eval returns the comment string.
+func (ce CommentExpr) Eval(_ Scope) (interface{}, error) {
+	return ce.comment, nil
+}
+
+func (ce CommentExpr) String() string {
+	return ce.comment
+}
+
+// KeywordExpr represents a keyword literal.
+type KeywordExpr struct {
+	Keyword string
+}
+
+// Eval returns the keyword itself.
+func (ke KeywordExpr) Eval(_ Scope) (interface{}, error) {
+	return ke.Keyword, nil
+}
+
+func (ke KeywordExpr) String() string {
+	return ke.Keyword
+}
+
+// SymbolExpr represents a symbol.
+type SymbolExpr struct {
+	Symbol string
+}
+
+// Eval returns the symbol name itself.
+func (se SymbolExpr) Eval(scope Scope) (interface{}, error) {
+	parts := strings.Split(se.Symbol, ".")
+	if len(parts) > 2 {
+		return nil, fmt.Errorf("invalid member access symbol. must be of format <parent>.<member>")
+	}
+
+	obj, err := scope.Get(parts[0])
+	if err != nil {
+		return nil, err
+	}
+
+	if len(parts) == 1 {
+		return obj, nil
+	}
+
+	member := resolveMember(reflect.ValueOf(obj), parts[1])
+	if !member.IsValid() {
+		return nil, fmt.Errorf("member '%s' not found on '%s'", parts[1], parts[0])
+	}
+
+	return member.Interface(), nil
+}
+
+func (se SymbolExpr) String() string {
+	return se.Symbol
+}
+
+func resolveMember(obj reflect.Value, name string) reflect.Value {
+	firstMatch := func(fxs ...func(string) reflect.Value) reflect.Value {
+		for _, fx := range fxs {
+			if val := fx(name); val.IsValid() && val.CanInterface() {
+				return val
+			}
+		}
+
+		return reflect.Value{}
+	}
+
+	var funcs []func(string) reflect.Value
+	if obj.Kind() == reflect.Ptr {
+		funcs = append(funcs,
+			obj.Elem().FieldByName,
+			obj.MethodByName,
+			obj.Elem().MethodByName,
+		)
+	} else {
+		funcs = append(funcs,
+			obj.FieldByName,
+			obj.MethodByName,
+		)
+	}
+
+	return firstMatch(funcs...)
+}
+
+// VectorExpr represents a vector form.
+type VectorExpr struct {
+	List []Expr
+}
+
+// Eval creates a golang slice.
+func (ve VectorExpr) Eval(scope Scope) (interface{}, error) {
+	lst := []interface{}{}
+
+	for _, expr := range ve.List {
+		val, err := expr.Eval(scope)
+		if err != nil {
+			return nil, err
+		}
+		lst = append(lst, val)
+	}
+
+	return lst, nil
+}
+
+func (ve VectorExpr) String() string {
+	strs := []string{}
+	for _, expr := range ve.List {
+		strs = append(strs, fmt.Sprint(expr))
+	}
+
+	return fmt.Sprintf("[%s]", strings.Join(strs, " "))
+}
+
+// ListExpr represents a list (i.e., a function call) expression.
+type ListExpr struct {
+	List []Expr
+}
+
+// Eval evaluates each s-exp in the list and then evaluates the list itself
+// as an s-exp.
+func (le ListExpr) Eval(scope Scope) (interface{}, error) {
+	if len(le.List) == 0 {
+		return le.List, nil
+	}
+
+	val, err := le.List[0].Eval(scope)
+	if err != nil {
+		return nil, err
+	}
+
+	if macroFn, ok := val.(MacroFunc); ok {
+		var name string
+		if sym, ok := le.List[0].(SymbolExpr); ok {
+			name = sym.Symbol
+		}
+		return macroFn(scope, name, le.List[1:])
+	}
+
+	args := []interface{}{}
+	for i := 1; i < len(le.List); i++ {
+		arg, err := le.List[i].Eval(scope)
+		if err != nil {
+			return nil, err
+		}
+		args = append(args, arg)
+	}
+
+	if scopedFn, ok := val.(ScopedFunc); ok {
+		return scopedFn(scope, args...)
+	}
+
+	return reflection.Call(val, args...)
+}
+
+func (le ListExpr) String() string {
+	reprs := []string{}
+	for _, item := range le.List {
+		reprs = append(reprs, fmt.Sprint(item))
+	}
+
+	return fmt.Sprintf("(%s)", strings.Join(reprs, " "))
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -30,6 +30,257 @@ func TestParse(suite *testing.T) {
 	})
 }
 
+func TestParse_Vector(suite *testing.T) {
+	suite.Parallel()
+
+	suite.Run("UnexpectedEOF", func(t *testing.T) {
+		expr, err := parser.Parse(reader("]"))
+		require.Error(t, err)
+		assert.Equal(t, io.EOF, err)
+		assert.Nil(t, expr)
+	})
+
+	suite.Run("UnexpectedEOF", func(t *testing.T) {
+		expr, err := parser.Parse(reader("["))
+		require.Error(t, err)
+		assert.Equal(t, io.EOF, err)
+		assert.Nil(t, expr)
+	})
+
+	suite.Run("EmptyList", func(t *testing.T) {
+		expr, err := parser.Parse(reader("[]"))
+		require.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.VectorExpr{}, expr)
+		assert.Equal(t, 0, len(expr.(parser.VectorExpr).List))
+	})
+
+	suite.Run("SimpleList", func(t *testing.T) {
+		expr, err := parser.Parse(reader("[1 \"hello\"]"))
+		require.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.VectorExpr{}, expr)
+		assert.Equal(t, 2, len(expr.(parser.VectorExpr).List))
+	})
+
+	suite.Run("NestedList", func(t *testing.T) {
+		expr, err := parser.Parse(reader("[1 [[] 'hello]]"))
+		require.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.VectorExpr{}, expr)
+		assert.Equal(t, 2, len(expr.(parser.VectorExpr).List))
+	})
+}
+
+func TestParse_Symbol(suite *testing.T) {
+	suite.Parallel()
+
+	suite.Run("AlphaSymbol", func(t *testing.T) {
+		expr, err := parser.Parse(reader("hello"))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.SymbolExpr{}, expr)
+		assert.Equal(t, "hello", expr.(parser.SymbolExpr).String())
+	})
+
+	suite.Run("AlphaNumericSymbol", func(t *testing.T) {
+		expr, err := parser.Parse(reader("hello123"))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.SymbolExpr{}, expr)
+		assert.Equal(t, "hello123", expr.(parser.SymbolExpr).String())
+	})
+
+	suite.Run("NonASCIISymbol", func(t *testing.T) {
+		expr, err := parser.Parse(reader("π"))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.SymbolExpr{}, expr)
+		assert.Equal(t, "π", expr.(parser.SymbolExpr).String())
+	})
+
+	suite.Run("NumerciSymbol", func(t *testing.T) {
+		expr, err := parser.Parse(reader("1.2.3"))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.SymbolExpr{}, expr)
+		assert.Equal(t, "1.2.3", expr.(parser.SymbolExpr).String())
+	})
+}
+
+func TestParse_String(suite *testing.T) {
+	suite.Parallel()
+
+	suite.Run("SimpleString", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`"hello"`))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.StringExpr{}, expr)
+		assert.Equal(t, "hello", expr.(parser.StringExpr).Value)
+	})
+
+	suite.Run("EscapeQuote", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`"hello\"world"`))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.StringExpr{}, expr)
+		assert.Equal(t, "hello\"world", expr.(parser.StringExpr).Value)
+	})
+
+	suite.Run("EscapeNewline", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`"hello\nworld"`))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.StringExpr{}, expr)
+		assert.Equal(t, "hello\nworld", expr.(parser.StringExpr).Value)
+	})
+
+	suite.Run("EscapeTab", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`"hello\tworld"`))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.StringExpr{}, expr)
+		assert.Equal(t, "hello\tworld", expr.(parser.StringExpr).Value)
+	})
+
+	suite.Run("EscapeCR", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`"hello\rworld"`))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.StringExpr{}, expr)
+		assert.Equal(t, "hello\rworld", expr.(parser.StringExpr).Value)
+	})
+
+	suite.Run("PrematureEOF", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`"hello`))
+		require.Error(t, err)
+		assert.Equal(t, io.EOF, err)
+		assert.Nil(t, expr)
+	})
+
+}
+
+func TestParse_Number(suite *testing.T) {
+	suite.Parallel()
+
+	suite.Run("NumberFollowedBySymbol", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`-12.34 hello`))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.NumberExpr{}, expr)
+		assert.Equal(t, "-12.34", expr.(parser.NumberExpr).String())
+	})
+
+	suite.Run("SimpleInteger", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`12`))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.NumberExpr{}, expr)
+		assert.Equal(t, "12", expr.(parser.NumberExpr).String())
+	})
+
+	suite.Run("Signed(-)Integer", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`-12`))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.NumberExpr{}, expr)
+		assert.Equal(t, "-12", expr.(parser.NumberExpr).String())
+	})
+
+	suite.Run("Signed(+)Integer", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`+12`))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.NumberExpr{}, expr)
+		assert.Equal(t, "+12", expr.(parser.NumberExpr).String())
+	})
+
+	suite.Run("SimpleFloat", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`+12.34`))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.NumberExpr{}, expr)
+		assert.Equal(t, "+12.34", expr.(parser.NumberExpr).String())
+	})
+
+	suite.Run("SignedFloat", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`-12.34`))
+		assert.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.NumberExpr{}, expr)
+		assert.Equal(t, "-12.34", expr.(parser.NumberExpr).String())
+	})
+}
+
+func TestParse_Keyword(suite *testing.T) {
+	suite.Parallel()
+
+	suite.Run("SimpleKeyword", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`:hello world`))
+		require.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.KeywordExpr{}, expr)
+		assert.Equal(t, "hello", expr.(parser.KeywordExpr).String())
+	})
+
+	suite.Run("ComplexKeyword", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`:hello/world`))
+		require.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.KeywordExpr{}, expr)
+		assert.Equal(t, "hello/world", expr.(parser.KeywordExpr).String())
+	})
+
+	suite.Run("SkipsEscape", func(t *testing.T) {
+		expr, err := parser.Parse(reader(`:hello\nworld`))
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "unexpected character")
+		assert.Nil(t, expr)
+	})
+}
+
+func TestParse_List(suite *testing.T) {
+	suite.Parallel()
+
+	suite.Run("UnexpectedEOF", func(t *testing.T) {
+		expr, err := parser.Parse(reader(")"))
+		require.Error(t, err)
+		assert.Equal(t, io.EOF, err)
+		assert.Nil(t, expr)
+	})
+
+	suite.Run("UnexpectedEOF", func(t *testing.T) {
+		expr, err := parser.Parse(reader("("))
+		require.Error(t, err)
+		assert.Equal(t, io.EOF, err)
+		assert.Nil(t, expr)
+	})
+
+	suite.Run("EmptyList", func(t *testing.T) {
+		expr, err := parser.Parse(reader("()"))
+		require.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.ListExpr{}, expr)
+		assert.Equal(t, 0, len(expr.(parser.ListExpr).List))
+	})
+
+	suite.Run("SimpleList", func(t *testing.T) {
+		expr, err := parser.Parse(reader("(1 \"hello\")"))
+		require.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.ListExpr{}, expr)
+		assert.Equal(t, 2, len(expr.(parser.ListExpr).List))
+	})
+
+	suite.Run("NestedList", func(t *testing.T) {
+		expr, err := parser.Parse(reader("(1 ([] 'hello))"))
+		require.NoError(t, err)
+		require.NotNil(t, expr)
+		require.IsType(t, parser.ListExpr{}, expr)
+		assert.Equal(t, 2, len(expr.(parser.ListExpr).List))
+	})
+}
+
 type readerFunc func([]byte) (int, error)
 
 func (rf readerFunc) Read(data []byte) (int, error) {
