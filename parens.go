0c2cfd85ee0f1db56866d5b23eaca9936650426a
Shivaprasad <shiv.ylp@gmail.com>
2020-09-10 10:24:40 +0530

    feat: add Evaluator implementation [#12]

--- parens.go
+++ parens.go
@@ -1,31 +1,60 @@
 package parens
 
-import "github.com/spy16/parens/value"
+// New returns a new instance of parens initialised based on given
+// options.
+func New(opts ...Option) *Evaluator {
+	p := &Evaluator{}
+	for _, opt := range withDefaults(opts) {
+		opt(p)
+	}
+	return p
+}
 
-// Evaluator provides the environment where forms are evaluated for result. Forms are
-// first converted to Expr based on pre-defined rules or custom analyzer rules
-// (See Analyzer).
-//
-// The zero value is ready to use.  Evaluator is safe for concurrent use.
+// Any represents any value.
+type Any interface{}
+
+// Evaluator represents an instance of parens interpreter.
 type Evaluator struct {
-	Analyzer
-	Expander
+	expander Expander
+	analyzer Analyzer
 }
 
-// Expr represents an expression that can be evaluated against the Evaluator.
-type Expr interface {
-	Eval(ctx Context, ev Evaluator) (value.Any, error)
+// Eval performs macro-expansion if necessary, converts the expanded form
+// to an expression and evaluates the resulting expression.
+func (e *Evaluator) Eval(form Any) (Any, error) {
+	expr, err := e.expandAnalyze(form)
+	if err != nil {
+		return nil, err
+	} else if expr == nil {
+		return nil, nil
+	}
+
+	return expr.Eval()
 }
 
-// Invokable represents a value that can be invoked for result.
-type Invokable interface {
-	Invoke(ev Evaluator, args ...value.Any) (value.Any, error)
+func (e *Evaluator) expandAnalyze(form Any) (Expr, error) {
+	if expr, ok := form.(Expr); ok {
+		// Already an Expr, nothing to do.
+		return expr, nil
+	}
+
+	if expanded, err := e.expander.Expand(e, form); err != nil {
+		return nil, err
+	} else if expanded != nil {
+		// Expansion did happen. Throw away the old form and continue with
+		// the expanded version.
+		form = expanded
+	}
+
+	return e.analyzer.Analyze(form)
 }
 
-// Analyzer implementation can be set on the Evaluator to override evaluation
-// rules for forms. See WithAnalyzer().
+// Analyzer implementation is responsible for performing syntax analysis
+// on given form.
 type Analyzer interface {
-	Analyze(ev Evaluator, form value.Any) (Expr, error)
+	// Analyze should perform syntax checks for special forms etc. and
+	// return Expr values that can be evaluated against a context.
+	Analyze(form Any) (Expr, error)
 }
 
 // Expander implementation is responsible for performing macro-expansion
@@ -34,55 +63,11 @@ type Expander interface {
 	// Expand should expand/rewrite the given form if it's a macro form
 	// and return the expanded version. If given form is not macro form,
 	// it should return nil, nil.
-	Expand(ev Evaluator, form value.Any) (value.Any, error)
+	Expand(p *Evaluator, form Any) (Any, error)
 }
 
-// Eval performs macro-expansion if necessary, converts the expanded form to an
-// expression and evaluates the resulting expression.
-func (ev Evaluator) Eval(ctx Context, form value.Any) (value.Any, error) {
-	if form == nil {
-		return nil, nil
-	}
-
-	expr, err := ev.expandAnalyze(form)
-	if err != nil {
-		return nil, err
-	} else if expr == nil {
-		return nil, nil
-	}
-
-	return expr.Eval(ctx, ev)
-}
-
-func (ev Evaluator) expandAnalyze(form value.Any) (Expr, error) {
-	if expr, ok := form.(Expr); ok {
-		// Already an Expr, nothing to do.
-		return expr, nil
-	}
-
-	if expanded, err := ev.expand(form); err != nil {
-		return nil, err
-	} else if expanded != nil {
-		// Expansion did happen. Throw away the old form and continue with
-		// the expanded version.
-		form = expanded
-	}
-
-	return ev.analyze(form)
-}
-
-func (ev Evaluator) expand(form value.Any) (value.Any, error) {
-	if ev.Expander == nil {
-		return basicExpander{}.Expand(ev, form)
-	}
-
-	return ev.Expander.Expand(ev, form)
-}
-
-func (ev Evaluator) analyze(form value.Any) (Expr, error) {
-	if ev.Analyzer == nil {
-		return BasicAnalyzer{}.Analyze(ev, form)
-	}
-
-	return ev.Analyzer.Analyze(ev, form)
+// Expr represents an expression that can be evaluated against a context.
+type Expr interface {
+	// TODO: (spy16) Modify signature as per Expr implementations.
+	Eval() (Any, error)
 }
--- parens_test.go
+++ parens_test.go
@@ -1,1 +1,18 @@
 package parens_test
+
+import (
+	"testing"
+
+	"github.com/spy16/parens"
+)
+
+func TestNew(t *testing.T) {
+	p := parens.New()
+	assertNotNil(t, p)
+}
+
+func assertNotNil(t *testing.T, v interface{}) {
+	if v == nil {
+		t.Errorf("wanted non-nil value, got nil")
+	}
+}
