9dbbdf4426b7dbce207d5d545419b55d4d6982c6
Shivaprasad <shiv.ylp@gmail.com>
2020-09-13 13:08:55 +0530

    Add symbol resolution

--- errors.go
+++ errors.go
@@ -6,6 +6,9 @@ import (
 )
 
 var (
+	// ErrNotFound is returned when a binding not found.
+	ErrNotFound = errors.New("not found")
+
 	// ErrInvalidBindName is returned by DefExpr when the bind name is invalid.
 	ErrInvalidBindName = errors.New("invalid name for def")
 
